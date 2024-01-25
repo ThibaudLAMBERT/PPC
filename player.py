e6dd100be37f89dd8fc9f42bb1e396c0c01e29e1
Thibaud LAMBERT <thibaud.lambert.25@gmail.com>
2024-01-25 18:04:14 +0100

    fonction gestion d'erreur

--- player.py
+++ player.py
@@ -20,7 +20,7 @@ def logo():
     print(" | |  | | (_| | | | | (_| | |_) | \__ \ ")
     print(" |_|  |_|\__,_|_| |_|\__,_|_.__/|_|___/")
 
-def communication(number_queue,data_queue,send_info):
+def communication(number_queue,card_queue,carte_drop_queue):
     HOST = "localhost"
     PORT = 6700
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -52,15 +52,50 @@ def communication(number_queue,data_queue,send_info):
         cartes = client_socket.recv(1024)
         new_cartes=cartes.decode()
      
-        data_queue.put(new_cartes)
-        data = send_info.get()
-        print(data)
-        client_socket.sendall(str(data.encode()))
+        card_queue.put(new_cartes)
+        card_drop = carte_drop_queue.get()
+        card_drop_send = str(card_drop)
+        client_socket.sendall(card_drop_send.encode())
+
+
+def gestion_erreur(message,choix):
+    if choix == 1:
+        while True: 
+            try:
+                reponse = input(message)
+                user_input = int(reponse)
+                assert user_input == 1 or user_input == 2
+                break
+
+            except ValueError:
+                print("Erreur: Ce n'est pas un nombre\n")
+
+            except AssertionError:
+                print("Le nombre doit être 1 ou 2\n")
+
+        return user_input
+    
+    elif choix == 2:
+        while True: 
+            try:
+                reponse = input(message)
+                user_input = int(reponse)
+                assert 0 < user_input < 5
+                break
+
+            except ValueError:
+                print("Erreur: Ce n'est pas un nombre\n")
+
+            except AssertionError:
+                print("Le nombre doit être entre 1 et 5\n")
+
+        return user_input
+
 
 
     
 
-def player(i, state, sem,nb_player,data_queue,newstdin,send_info):
+def player(i, state, sem,nb_player,card_queue,newstdin,carte_drop_queue):
     while game:
         if state[i] == 1:
             print(f"Le Player {i+1} va jouer ")
@@ -68,7 +103,7 @@ def player(i, state, sem,nb_player,data_queue,newstdin,send_info):
             sem.release()
             state[i] = 0
             sem.acquire()
-            carte = data_queue.get()
+            carte = card_queue.get()
             list_mains = ast.literal_eval(carte)
             for joueur_index in range(nb_player):
                 if joueur_index != i:
@@ -76,39 +111,16 @@ def player(i, state, sem,nb_player,data_queue,newstdin,send_info):
                     print(list_mains[joueur_index])
                     print()
             sys.stdin = newstdin
-            while True: 
-                try:
-                    reponse = input("Tapez 1 pour jeter une carte, tapez 2 pour utiliser un jeton d'information : ")
-                    choix = int(reponse)
-                    assert choix == 1 or choix == 2
-                    break
-
-                except ValueError:
-                    print("Erreur: Ce n'est pas un nombre\n")
-
-                except AssertionError:
-                    print("Le nombre doit être 1 ou 2\n")
+            choix = gestion_erreur("Tapez 1 pour jeter une carte, tapez 2 pour utiliser un jeton d'information : ",1)
 
             if choix == 1:
                 print("Vous avez choisis de jeter une carte")
                 sys.stdin = newstdin
-
-                while True: 
-                    try:
-                        reponse2 = input("Quelle carte voulez vous jeter, donnez l'indice : ")
-                        choix2 = int(reponse)
-                        assert 0 < choix2 < 5
-                        break
-
-                    except ValueError:
-                        print("Erreur: Ce n'est pas un nombre\n")
-
-                    except AssertionError:
-                        print("Le nombre doit être entre 1 et 5\n")
+                choix2 = gestion_erreur("Quelle carte voulez vous jeter, donnez l'indice : ",2)
 
                 print(f"Vous avez choisis de jeter la carte {list_mains[i][choix2-1]}")
 
-                send_info.put([i, choix2])
+                carte_drop_queue.put([i, choix2])
 
             elif choix == 2:
                 print("Vous avez choisis d'utiliser un token d'information")
@@ -124,9 +136,9 @@ if __name__ == "__main__":
     clear()
     logo()
     player_queue = queue.Queue()
-    shared_data_queue = queue.Queue()
-    send_info = multiprocessing.Queue()
-    thread_communication = threading.Thread(target=communication,args=(player_queue,shared_data_queue,send_info))
+    card_queue = queue.Queue()
+    card_drop_queue = multiprocessing.Queue()
+    thread_communication = threading.Thread(target=communication,args=(player_queue,card_queue,card_drop_queue))
     thread_communication.start()
     newstdin = os.fdopen(os.dup(sys.stdin.fileno()))
     nb_player = player_queue.get()
@@ -137,13 +149,11 @@ if __name__ == "__main__":
     
     sem = multiprocessing.Semaphore(0)
 
-    processes = [multiprocessing.Process(target=player, args=(i, state, sem,nb_player,shared_data_queue,newstdin,send_info)) for i in range(nb_player)]
+    processes = [multiprocessing.Process(target=player, args=(i, state, sem,nb_player,card_queue,newstdin,card_drop_queue)) for i in range(nb_player)]
 
     for process in processes:
         process.start()
 
-    value = send_info.get()
-    print(value)
 
     for process in processes:
         process.join()
