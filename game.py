7ab2aff285882fd7aa1bad98dcc699ba78120bc7
Antoskuu <antoning.antonin@gmail.com>
2024-01-27 17:55:11 +0100

    finished requete

--- game.py
+++ game.py
@@ -141,7 +141,10 @@ def main(index, shared_memory):
                     
                     player_requete = requete[1]
                     index_card = requete[2]
-                    if mains[player_requete][index_card]
+                    if mains[player_requete][index_card][0]==pile[couleurToIndice([player_requete][index_card][1])]-1:
+                        pile[couleurToIndice([player_requete][index_card][1])]+=1
+                        
+                        
                     shared_memory[0]-=1
                     #print("Il a choisis de jeter une carte")
                     player_requete = requete[1]
